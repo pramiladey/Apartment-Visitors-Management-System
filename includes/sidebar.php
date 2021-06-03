462c43057a4a42ba99b0e692e4c01120aa7f643a
pramiladey <85215957+pramiladey@users.noreply.github.com>
2021-06-03 22:19:57 +0530

    Add files via upload

---
+++ includes/sidebar.php
@@ -0,0 +1,32 @@
+<aside class="menu-sidebar d-none d-lg-block" style="background-color: lightcoral;">
+            <div class="logo" style="background-color: lightcoral;">
+        
+                   <h1>Apartment visitors</h1>
+            </div>
+            <div class="menu-sidebar__content js-scrollbar1" style="background-color: lightcoral;">
+                <nav class="navbar-sidebar">
+                    <ul class="list-unstyled navbar__list">
+                        <li>
+                            <a href="dashboard.php"  style="color: black">
+                                <i class="fas fa-tachometer-alt"></i>Dashboard</a>
+                        </li>
+
+     <li>
+                            <a href="visitors-form.php"  style="color: black">
+                                <i class="fa fa-user"></i>New Visitor</a>
+                        </li>
+   <li>
+                            <a href="manage-newvisitors.php"  style="color: black">
+                                <i class="fa fa-users"></i>Manage Visitors</a>
+                        </li>
+
+                        
+                      <li>
+                            <a href="bwdates-reports.php"  style="color: black">
+                                <i class="fas fa-copy"></i>Vistors B/w Dates</a>
+                        </li>  
+                       
+                    </ul>
+                </nav>
+            </div>
+        </aside>
